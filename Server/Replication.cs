ec9c1e28260dfa8f6ed1e02cd2f3d24ec0da11e8
Hussein Al-kaf <hyalkaf@ucalgary.ca>
2016-03-27 22:20:41 -0600

    Changed ip address to broadcasting again

--- Server/Replication.cs
+++ Server/Replication.cs
@@ -35,7 +35,7 @@ namespace Server
         // Udp client listening for broadcast messages
         private readonly UdpClient udpBroadcast = new UdpClient(15000);
         // IP Address for broadcasting
-        IPEndPoint sendingIP = new IPEndPoint(IPAddress.Parse("10.2.0.10"), 15000);
+        IPEndPoint sendingIP = new IPEndPoint(IPAddress.Broadcast, 15000);
         IPEndPoint receivingIP = new IPEndPoint(IPAddress.Any, 0);
 
         // Request messsages between replicas and server
