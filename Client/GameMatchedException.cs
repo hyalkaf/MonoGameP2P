fa4806808cbeb517497d0f18e1e12f2d9bc1ce65
Chih-Hsuan Huang <gemhuang2151992@gmail.com>
2016-03-30 01:05:33 -0600

    attempting to fix incorrect connected client list

---
+++ Client/GameMatchedException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Client
+{
+    [Serializable]
+    internal class GameMatchedException : Exception
+    {
+        public GameMatchedException()
+        {
+        }
+
+        public GameMatchedException(string message) : base(message)
+        {
+        }
+
+        public GameMatchedException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected GameMatchedException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
