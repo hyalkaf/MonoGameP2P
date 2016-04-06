3c1577ac5d69dba5b0434dc6766fda218e345343
Hussein Al-kaf <hyalkaf@ucalgary.ca>
2016-04-06 01:04:16 -0600

    was sending to itself

--- Server/ReplicationManager.cs
+++ Server/ReplicationManager.cs
@@ -227,7 +227,7 @@ namespace Server
                     backupClient.Close();
 
                     // Send to all backups
-                    IEnumerable<IPAddress> backupsIPs = serversAddresses.SkipWhile((backup, indexOfBackup) => indexOfBackup != 0);
+                    IEnumerable<IPAddress> backupsIPs = serversAddresses.SkipWhile((backup, indexOfBackup) => indexOfBackup != 0 && !backup.Equals(thisServer.IPAddr));
                     if (SendToReplicationManagers(backupsIPs, responseMessage))
                     {
                         // Send to everybody the new state if something changed.
