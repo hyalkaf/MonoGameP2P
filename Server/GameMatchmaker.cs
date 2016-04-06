b3a74d6037fb3d160110d7c848aa8ff10fbaed29
Hussein Al-kaf <hyalkaf@ucalgary.ca>
2016-04-06 05:14:39 -0600

    quarding tcpClient in all needed places

--- Server/GameMatchmaker.cs
+++ Server/GameMatchmaker.cs
@@ -256,7 +256,7 @@ namespace Server
                             }
                         }
 
-                        Console.WriteLine("DEBUG: client " + client.PlayerName + client.IPAddr + " connected is " + client.TcpClient.Client.Connected);
+                        Console.WriteLine("DEBUG: client " + client.PlayerName + client.IPAddr + " connected is " + client.TcpClient != null ? client.TcpClient.Client.Connected.ToString() : "false");
                     }
                     // If amount of current connected players is not sufficient to form a game, returns.
                     if (stillConnected < i) return;
@@ -279,7 +279,7 @@ namespace Server
                        
 
                         // Assign the ip address to a port
-                        if (client.TcpClient.Client.Connected)
+                        if (client.TcpClient != null && client.TcpClient.Client.Connected)
                         {
                             client.PlayerId = playerId;
                             client.ListeningPort = portNumber++;
